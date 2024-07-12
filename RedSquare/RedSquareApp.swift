4bf0569796d90241a90113ecd280eddb5bf13b80
AndreyVZav <zav.av@bk.ru>
2024-07-12 14:46:35 +0300

    Initial Commit

---
+++ RedSquare/RedSquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedSquareApp.swift
+//  RedSquare
+//
+//  Created by Андрей Завадский on 12.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RedSquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
